6afef19b3e3233882f6a86df7fe2bcd3e5351ae9
Jeff Kent <jakent@gmail.com>
2011-02-24 22:51:49 -0600

    pollux_ego: cleaned up config file

--- include/configs/pollux_ego.h
+++ include/configs/pollux_ego.h
@@ -26,33 +26,34 @@
 #ifndef __CONFIG_H
 #define __CONFIG_H
 
-#define DEBUG
+/*#define DEBUG*/
 
+#define POLLUX_EGO
+
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#define CONFIG_SYS_NO_FLASH
+#define CONFIG_SYS_CONSOLE_INFO_QUIET
+#define CONFIG_SKIP_RELOCATE_UBOOT
+#define CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_SIZE			(32 << 10)	/* 32 KiB */
+
+/* SoC configuration */
 #define CONFIG_ARM926EJS
-#define CONFIG_POLLUX			/* SOC family name */
-#define CONFIG_VR3520F			/* SOC name */
+#define CONFIG_POLLUX			/* SoC family name */
+#define CONFIG_VR3520F			/* SoC name */
+#define CONFIG_SYS_HZ_PLL0		528000000
+#define CONFIG_SYS_HZ_PLL1		147461538
+#define CONFIG_SYS_HZ			1024
 
 #include <asm/arch/uart.h>
 #include <asm/arch/timer.h>
 #include <asm/arch/nand.h>
 
-/* minimal support for now, so turn lots of stuff off */
-#define CONFIG_SYS_NO_FLASH
-#define CONFIG_SKIP_RELOCATE_UBOOT
-#define CONFIG_SKIP_LOWLEVEL_INIT
-
-/* memory layout */
+/* memory info */
 #define CONFIG_NR_DRAM_BANKS		1
 #define PHYS_SDRAM_1			0x00000000
 #define PHYS_SDRAM_1_SIZE		(128 << 20)	/* 128 MiB */
-#define CONFIG_SYS_GBL_DATA_SIZE	128
-#define CONFIG_ENV_IS_IN_NAND
-#define CONFIG_ENV_SIZE			(32 << 10)	/* 32 KiB */
-#define CONFIG_ENV_OFFSET		0x00380000
-#define CONFIG_SYS_MALLOC_LEN		(CONFIG_ENV_SIZE + 1024 * 1024)
-#define CONFIG_STACKSIZE		(128 * 1024)
 #define CONFIG_SYS_TEXT_BASE		0x01000000
-#define CONFIG_SYS_LOAD_ADDR		0x00008000
 
 /* timer set up */
 #define CONFIG_SYS_TIMERBASE		TIMER0_BASE
@@ -60,21 +61,14 @@
 #define CONFIG_SYS_TIMER_CLKDIV		35
 #define CONFIG_SYS_TIMER_SELTCLK_LOG2	2
 
-/* clock set up */
-#define CONFIG_SYS_HZ_PLL0		528000000
-#define CONFIG_SYS_HZ_PLL1		147461538
-#define CONFIG_SYS_HZ			1024
-
 /* serial port configuration */
 #define CONFIG_S3C44B0_SERIAL
-#define CONFIG_SYS_UART_PLL		1
 #define CONFIG_BAUDRATE			115200
 #define CONFIG_SYS_BAUDRATE_TABLE	{ 9600, 19200, 38400, 57600, 115200 }
 #define CONFIG_SYS_BAUDRATE_MAX		115200
+#define CONFIG_SYS_UART_PLL		1
 
 /* ethernet configuration */
-#define CONFIG_CMD_NET
-#define CONFIG_NET_MULTI
 #define CONFIG_DRIVER_DM9000
 #define CONFIG_DM9000_BASE		0x84000000 /* cs1, shadow = 1 */
 #define DM9000_IO			CONFIG_DM9000_BASE
@@ -82,34 +76,76 @@
 
 /* nand configuration */
 #define CONFIG_NAND_POLLUX
-#define CONFIG_SYS_NAND_SW_ECC
-#define CONFIG_SYS_NAND_BASE		NAND_SHADOW_BASE
-#define CONFIG_SYS_NAND_QUIET_TEST
 #define CONFIG_SYS_MAX_NAND_DEVICE	1
-/*#define CONFIG_MTD_DEBUG
-#define CONFIG_MTD_DEBUG_VERBOSE	3*/
-
-#define CONFIG_CMD_MTDPARTS
-#define CONFIG_MTD_PARTITIONS
-#define CONFIG_MTD_DEVICE
-#define CONFIG_CMD_NAND
-#define CONFIG_CMD_UBI
-#define CONFIG_RBTREE
-#define CONFIG_CMD_NAND_LOCK_UNLOCK
-#define CONFIG_CMD_SAVEENV
+#define CONFIG_SYS_NAND_BASE		NAND_SHADOW_BASE
+#define CONFIG_SYS_NAND_CS              2
+#define CONFIG_SYS_NAND_QUIET_TEST
+#define CONFIG_SYS_NAND_SW_ECC
+#define CONFIG_SYS_NAND_USE_FLASH_BBT
 
 /* console/printing setup */
-#define CONFIG_SYS_PROMPT "=> "
+#define CONFIG_AUTO_COMPLETE
+#define CONFIG_CMD_ECHO
+#define CONFIG_CMDLINE_EDITING
+#define CONFIG_SYS_HUSH_PARSER
+#define CONFIG_SYS_PROMPT		"U-Boot# "
+#define CONFIG_SYS_PROMPT_HUSH_PS2	"> "
 #define CONFIG_SYS_LONGHELP
-#define CONFIG_SYS_CBSIZE		256
-#define CONFIG_SYS_PBSIZE \
+#define CONFIG_SYS_CBSIZE		1024
+#define CONFIG_SYS_PBSIZE 		\
 	(CONFIG_SYS_CBSIZE + sizeof(CONFIG_SYS_PROMPT) + 16)
 #define CONFIG_SYS_MAXARGS		16
 
-#define CONFIG_MAX_RAM_BANK_SIZE        (128 << 20)	/* 128 MB */
+/* nand features */
+#ifdef CONFIG_NAND_POLLUX
+#define CONFIG_CMD_MTDPARTS
+#define CONFIG_CMD_NAND
+#define CONFIG_CMD_NAND_LOCK_UNLOCK
+#define CONFIG_CMD_UBI
+/*#define CONFIG_MTD_DEBUG
+#define CONFIG_MTD_DEBUG_VERBOSE	3*/
+#define CONFIG_MTD_DEVICE
+#define CONFIG_MTD_PARTITIONS
+#define CONFIG_RBTREE
+#endif
 
-#define CONFIG_SYS_SDRAM_BASE           PHYS_SDRAM_1
-#define CONFIG_SYS_INIT_SP_ADDR \
+/* nand environment */
+#ifdef CONFIG_NAND_POLLUX
+#undef CONFIG_ENV_IS_NOWHERE
+#undef CONFIG_ENV_SIZE
+#define CONFIG_ENV_SIZE			(256 << 10)	/* 256 KiB */
+#define CONFIG_ENV_IS_IN_NAND
+#define CONFIG_ENV_OFFSET		0x00380000
+#define CONFIG_ENV_OVERWRITE
+#define CONFIG_CMD_SAVEENV
+#endif /* CONFIG_NAND_POLLUX */
+
+/* other environment */
+#define CONFIG_CMD_ASKENV
+#define CONFIG_CMD_EDITENV
+#define CONFIG_CMD_RUN
+
+/* networking */
+#ifdef CONFIG_DRIVER_DM9000
+#define CONFIG_CMD_DHCP
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_NET
+#define CONFIG_NET_MULTI
+#define CONFIG_NET_RETRY_COUNT		10
+#endif /* CONFIG_DRIVER_DM9000 */
+
+/* Linux interfacing */
+#define CONFIG_CMDLINE_TAG
+#define CONFIG_SETUP_MEMORY_TAGS
+#define CONFIG_SYS_BARGSIZE		1024
+#define CONFIG_SYS_LOAD_ADDR		0x00008000
+
+/* U-Boot memory configuration */
+#define CONFIG_STACKSIZE		(256 << 10)	/* 256 KiB */
+#define CONFIG_SYS_MALLOC_LEN		(1 << 20)	/* 1 MiB */
+#define CONFIG_MAX_RAM_BANK_SIZE	(128 << 20)	/* 128 MB */
+#define CONFIG_SYS_SDRAM_BASE		PHYS_SDRAM_1
+#define CONFIG_SYS_INIT_SP_ADDR		\
 	(CONFIG_SYS_SDRAM_BASE + 0x8000 - GENERATED_GBL_DATA_SIZE)
 
 #endif	/* __CONFIG_H */
